1b47130f76100d553858a26a8922e4bf419c30d3
Roberto Rojas <rrojas@chariotsolutions.com>
2016-07-08 20:49:27 -0400

    Added sample client

---
+++ pets/dogs-service/client/main.go
@@ -0,0 +1,122 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/robertojrojas/microservices-go/pets/dogs-service/messaging"
+	"github.com/streadway/amqp"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+
+func randomString(l int) string {
+	bytes := make([]byte, l)
+	for i := 0; i < l; i++ {
+		bytes[i] = byte(randInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func randInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func readAllRPC(n int) (res int, err error) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when usused
+		true,  // exclusive
+		false, // noWait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError(err, "Failed to register a consumer")
+
+	corrId := randomString(32)
+
+	rpcRequest := &messaging.RPCMessage{
+		Type: messaging.ReadAllMessage,
+		Data: []byte(""),
+	}
+
+	rpcData, err := json.Marshal(rpcRequest)
+	failOnError(err, "Failed to Marshal rpcRequest")
+
+	err = ch.Publish(
+		"",          // exchange
+		"rpc_queue", // routing key
+		false,       // mandatory
+		false,       // immediate
+		amqp.Publishing{
+			ContentType:   "text/plain",
+			CorrelationId: corrId,
+			ReplyTo:       q.Name,
+			Body:          rpcData,
+		})
+	failOnError(err, "Failed to publish a message")
+
+	for d := range msgs {
+		// if corrId == d.CorrelationId {
+		// 	res, err = strconv.Atoi(string(d.Body))
+		// 	failOnError(err, "Failed to convert body to integer")
+		// 	break
+		// }
+		fmt.Printf("%#v\n", d)
+	}
+
+	return
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	n := bodyFrom(os.Args)
+
+	log.Printf(" [x] Requesting fib(%d)", n)
+	res, err := readAllRPC(n)
+	failOnError(err, "Failed to handle RPC request")
+
+	log.Printf(" [.] Got %d", res)
+}
+
+func bodyFrom(args []string) int {
+	var s string
+	if (len(args) < 2) || os.Args[1] == "" {
+		s = "30"
+	} else {
+		s = strings.Join(args[1:], " ")
+	}
+	n, err := strconv.Atoi(s)
+	failOnError(err, "Failed to convert arg to integer")
+	return n
+}
