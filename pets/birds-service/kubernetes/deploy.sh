0a7648946cce0c2c1fda3fbd7bcdc2bc0eaf6eb1
Roberto J Rojas <robertojrojas@gmail.com>
2016-12-28 11:04:54 -0500

    added k8s configmap

--- pets/birds-service/kubernetes/deploy.sh
+++ pets/birds-service/kubernetes/deploy.sh
@@ -1,2 +1,2 @@
-kubectl create configmap postgresql-config --from-literal=postgresql.uri=postgres://birdman:mycape@10.0.1.122/pqgotest?sslmode=disable
+kubectl create -f postgresql-configmap.yaml
 kubectl create -f birds-service-pod.yaml
