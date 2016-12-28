24fc04bd78014291e7a07c651066bdce94be8bae
Roberto J Rojas <robertojrojas@gmail.com>
2016-12-28 14:54:52 -0500

    pets-service: started to dockerize

--- pets/pets-service/main.go
+++ pets/pets-service/main.go
@@ -6,21 +6,35 @@ import (
 
 	"github.com/robertojrojas/microservices-go/pets/pets-service/executor"
 	"github.com/robertojrojas/microservices-go/pets/pets-service/services"
+	"os"
 )
+
+type config struct {
+	catsServiceURI string
+	birdsServiceURI string
+	rabbitMQURI   string
+	rabbitMQQueue string
+}
 
 func main() {
 
+	appConfig := getConfig()
+
 	catService := &services.CatService{
-		URL: "http://localhost:8091/api/cats",
+		URL: appConfig.catsServiceURI,
 	}
 	birdsService := &services.BirdService{
-		ServiceAddress: "localhost:8092",
+		ServiceAddress: appConfig.birdsServiceURI,
 	}
+
 	dogsService := &services.DogsService{
-		ServiceAddress: "amqp://guest:guest@localhost:5672/",
-		RPCQueue:       "dog_service_rpc_queue",
+		ServiceAddress: appConfig.rabbitMQURI,
+		RPCQueue:       appConfig.rabbitMQQueue,
 	}
 	rpcExecutor := executor.NewRPCExecutor(catService, birdsService, dogsService)
+
+
+
 	results, err := rpcExecutor.GetAllPets()
 	if err != nil {
 		log.Fatal(err)
@@ -32,4 +46,31 @@ func main() {
 	}
 	log.Printf("%s\n", output)
 
+}
+
+func getConfig() *config {
+
+	envConfig := config{}
+
+	envConfig.rabbitMQURI = os.Getenv("RABBITMQ_URI")
+	if envConfig.rabbitMQURI == "" {
+		envConfig.rabbitMQURI = "amqp://guest:guest@localhost:5672/"
+	}
+
+	envConfig.rabbitMQQueue = os.Getenv("RABBITMQ_QUEUE")
+	if envConfig.rabbitMQQueue == "" {
+		envConfig.rabbitMQQueue = "dog_service_rpc_queue"
+	}
+
+	envConfig.catsServiceURI = os.Getenv("CATS_SERVICE_URI")
+	if envConfig.catsServiceURI == "" {
+		envConfig.catsServiceURI = "http://cats-service-svc:8091/api/cats"
+	}
+
+	envConfig.birdsServiceURI = os.Getenv("BIRDS_SERVICE_URI")
+	if envConfig.birdsServiceURI == "" {
+		envConfig.birdsServiceURI = "birds-service-svc:8092"
+	}
+
+	return &envConfig
 }
