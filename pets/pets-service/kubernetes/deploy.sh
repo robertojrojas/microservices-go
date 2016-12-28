418755971fe9ed00bbb1ebd09dcca8ea5e293a22
Roberto J Rojas <robertojrojas@gmail.com>
2016-12-28 16:15:37 -0500

    pets-service: k8s ready

---
+++ pets/pets-service/kubernetes/deploy.sh
@@ -0,0 +1,5 @@
+kubectl create -f rabbitmq-configmap.yml
+kubectl create -f birds-service-configmap.yml
+kubectl create -f cats-service-configmap.yml
+kubectl create -f pets-service-pod.yml
+kubectl create -f pets-service-svc.yml
