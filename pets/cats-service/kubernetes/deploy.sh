05f6217aabd64eb10603681a0e575187501456c9
Roberto J Rojas <robertojrojas@gmail.com>
2016-12-28 22:53:53 -0500

    fixed typos

--- pets/cats-service/kubernetes/deploy.sh
+++ pets/cats-service/kubernetes/deploy.sh
@@ -1,3 +1,3 @@
-kubectl create -f mysql-config.yml
+kubectl create -f mysql-configmap.yml
 kubectl create -f cats-service-pod.yml
 kubectl create -f cats-service-svc.yml
--- pets/pets-service/kubernetes/deploy.sh
+++ pets/pets-service/kubernetes/deploy.sh
@@ -1,4 +1,5 @@
 kubectl create -f rabbitmq-configmap.yml
+kubectl create -f redis-configmap.yml
 kubectl create -f birds-service-configmap.yml
 kubectl create -f cats-service-configmap.yml
 kubectl create -f pets-service-pod.yml
